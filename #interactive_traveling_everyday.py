b2f5fc17cd922db00ba280b89eee2c55036de244
Award9 <91084966+Award9@users.noreply.github.com>
2024-09-14 01:27:35 -0400

    Add files via upload

---
+++ #interactive_traveling_everyday.py
@@ -0,0 +1,25 @@
+#interactive traveling everyday
+day_number = 0
+given_season = ['Fall', 'Winter', 'Spring', 'Summer']
+#for season in given_season:
+    
+#date = print(day_number," day, " season)
+
+#import values of everyday time
+time_of_day = 0 
+increasing_int = 1
+
+#Wake up in your room
+#options 
+#1. check the weather
+#2. check your voicemail
+#3. check your email
+#4. check your text message
+#5. look out the window onto the street
+#6. get dressed and leave
+
+#outside the house on mainstreet 
+#where everyday begins
+
+#options
+#
